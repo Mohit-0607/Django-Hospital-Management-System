2a98180e932cb9238d6490fd2f463a50f4d5f4f1
Mohit <77422525+Mohit-0607@users.noreply.github.com>
2021-07-02 22:51:37 +0530

    Delete DOCTER directory

--- DOCTER/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from DOCTER.models import *
-admin.site.register(Docter)
-admin.site.register(Prescription2)
--- DOCTER/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocterConfig(AppConfig):
-    name = 'DOCTER'
